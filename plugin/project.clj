c88ced5fe1c2f297e5f7d4ee4f2217b1001597c2
Moe Aboulkheir <moe.aboulkheir@gmail.com>
2015-07-05 23:00:17 +0100

    eh, 0.2.0

--- plugin/project.clj
+++ plugin/project.clj
@@ -1,4 +1,4 @@
-(defproject io.nervous/lein-cljs-lambda "0.1.3-SNAPSHOT"
+(defproject io.nervous/lein-cljs-lambda "0.2.0-SNAPSHOT"
   :description "Deploying Clojurescript functions to AWS Lambda"
   :url "https://github.com/nervous-systems/cljs-lambda"
   :license {:name "Unlicense" :url "http://unlicense.org/UNLICENSE"}
