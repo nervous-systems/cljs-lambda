7a2016dead09ad1e4aa11c9ac0da56b07e24d922
Moe Aboulkheir <moe.aboulkheir@gmail.com>
2015-08-10 12:01:05 +0100

    0.2.4 template

--- template/project.clj
+++ template/project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-lambda/lein-template "0.2.3"
+(defproject cljs-lambda/lein-template "0.2.4"
   :description "Clojurescript on AWS Lambda"
   :url "https://github.com/nervous-systems/cljs-lambda"
   :license {:name "Unlicense" :url "http://unlicense.org/UNLICENSE"}
