3abb3a4afc9ee8df008ae8ce16332e5054122fc0
Moe Aboulkheir <moe.aboulkheir@gmail.com>
2015-08-10 11:59:17 +0100

    Bump version, :expose -> :export in template

--- template/src/leiningen/new/cljs_lambda/core.cljs
+++ template/src/leiningen/new/cljs_lambda/core.cljs
@@ -2,7 +2,7 @@
   (:require [cljs-lambda.util :refer [async-lambda-fn]])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
-(def ^:expose work-magic
+(def ^:export work-magic
   (async-lambda-fn
    (fn [{:keys [variety]} context]
      (go
