f1970f03984ac347a226bfed89940e8d68a4d185
Moe Aboulkheir <moe.aboulkheir@gmail.com>
2016-11-08 18:39:49 +0000

    Update dependencies

--- cljs-lambda/project.clj
+++ cljs-lambda/project.clj
@@ -1,18 +1,18 @@
-(defproject io.nervous/cljs-lambda "0.3.2"
+(defproject io.nervous/cljs-lambda "0.3.3-SNAPSHOT"
   :description "Clojurescript AWS Lambda utilities"
   :url "https://github.com/nervous-systems/cljs-lambda"
   :license {:name "Unlicense" :url "http://unlicense.org/UNLICENSE"}
   :scm {:name "git" :url "https://github.com/nervous-systems/cljs-lambda"}
   :dependencies [[org.clojure/clojure            "1.8.0"]
-                 [org.clojure/clojurescript      "1.8.34"]
-                 [org.clojure/core.async         "0.2.374"]
+                 [org.clojure/clojurescript      "1.8.51"]
+                 [org.clojure/core.async         "0.2.395"]
                  [org.clojure/tools.macro        "0.1.2"]
-                 [funcool/promesa                "1.1.1"]
+                 [funcool/promesa                "1.6.0"]
                  [io.nervous/cljs-nodejs-externs "0.2.0"]]
-  :plugins [[lein-doo       "0.1.7-SNAPSHOT"]
-            [lein-npm       "0.6.0"]
-            [lein-cljsbuild "1.1.2"]
-            [lein-codox     "0.9.4"]]
+  :plugins [[lein-doo       "0.1.7"]
+            [lein-npm       "0.6.2"]
+            [lein-cljsbuild "1.1.4"]
+            [lein-codox     "0.10.1"]]
   ;; Codox can't deal w/ deps.cljs, so isolate externs
   :source-paths ["src" "src-deps"]
   :cljsbuild
--- plugin/project.clj
+++ plugin/project.clj
@@ -2,8 +2,8 @@
   :description "Deploying Clojurescript functions to AWS Lambda"
   :url "https://github.com/nervous-systems/cljs-lambda"
   :license {:name "Unlicense" :url "http://unlicense.org/UNLICENSE"}
-  :dependencies [[lein-cljsbuild "1.0.6"]
-                 [lein-npm       "0.5.0"]
+  :dependencies [[lein-cljsbuild "1.1.4"]
+                 [lein-npm       "0.6.2"]
                  [base64-clj     "0.1.1"]
                  [de.ubercode.clostache/clostache "1.4.0"]
                  [org.apache.commons/commons-compress "1.11"]
