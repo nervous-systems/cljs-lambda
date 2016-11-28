38b01dfb9a26f1b59b782c719a1ede36acdb1d73
Moe Aboulkheir <moe@nervous.io>
2016-11-28 10:49:35 +0000

    0.6.3 (#53)
    * Update dependencies
    
    * Fix issue with context/environment
    
    * Update template dependencies
    
    * 0.6.3

--- plugin/project.clj
+++ plugin/project.clj
@@ -1,9 +1,9 @@
-(defproject io.nervous/lein-cljs-lambda "0.6.3-SNAPSHOT"
+(defproject io.nervous/lein-cljs-lambda "0.6.3"
   :description "Deploying Clojurescript functions to AWS Lambda"
   :url "https://github.com/nervous-systems/cljs-lambda"
   :license {:name "Unlicense" :url "http://unlicense.org/UNLICENSE"}
-  :dependencies [[lein-cljsbuild "1.0.6"]
-                 [lein-npm       "0.5.0"]
+  :dependencies [[lein-cljsbuild "1.1.4"]
+                 [lein-npm       "0.6.2"]
                  [base64-clj     "0.1.1"]
                  [de.ubercode.clostache/clostache "1.4.0"]
                  [org.apache.commons/commons-compress "1.11"]
