461e8b0c5a56e18ba54aaaca5e5817168039a8a3
Moe Aboulkheir <moe@nervous.io>
2017-02-06 10:52:34 +0000

    0.6.4 (#59)
    * Bump versions
    
    * Regenerate example project

--- templates/serverless/src/leiningen/new/serverless_cljs/project.clj
+++ templates/serverless/src/leiningen/new/serverless_cljs/project.clj
@@ -1,10 +1,10 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure       "1.8.0"]
                  [org.clojure/clojurescript "1.8.51"]
-                 [io.nervous/cljs-lambda    "0.3.4-SNAPSHOT"]]
+                 [io.nervous/cljs-lambda    "0.3.4"]]
   :plugins [[lein-cljsbuild "1.1.5"]
             [lein-npm       "0.6.2"]
-            [io.nervous/lein-cljs-lambda "0.6.4-SNAPSHOT"]]
+            [io.nervous/lein-cljs-lambda "0.6.4"]]
   :cljsbuild
   {:builds [{:id "{{name}}"
              :source-paths ["src"]
