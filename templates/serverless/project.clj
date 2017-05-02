823c15bb565c2364ed197a87fe44d56fa8fd393e
Moe Aboulkheir <moe@nervous.io>
2017-05-02 12:23:40 +0100

    0.6.6

--- templates/serverless/project.clj
+++ templates/serverless/project.clj
@@ -1,4 +1,4 @@
-(defproject serverless-cljs/lein-template "0.1.3"
+(defproject serverless-cljs/lein-template "0.1.4"
   :description "Clojurescript on AWS Lambda via Serverless"
   :url         "https://github.com/nervous-systems/cljs-lambda"
   :license     {:name "Unlicense" :url "http://unlicense.org/UNLICENSE"}
