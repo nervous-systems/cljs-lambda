8bde5246d8e1c4b994bfdf44cf682e545cc4c96c
Moe Aboulkheir <moe@nervous.io>
2017-05-12 20:02:39 +0100

    0.6.6 (#80)
    * Undo index-advanced change; assume toplevel NS
    
    * 0.6.6

--- example/project.clj
+++ example/project.clj
@@ -8,7 +8,7 @@
   :plugins [[lein-cljsbuild "1.1.4"]
             [lein-npm       "0.6.0"]
             [lein-doo       "0.1.7"]
-            [io.nervous/lein-cljs-lambda "0.6.5"]]
+            [io.nervous/lein-cljs-lambda "0.6.6"]]
   :npm {:dependencies [[source-map-support "0.4.0"]]}
   :source-paths ["src"]
   :cljs-lambda
